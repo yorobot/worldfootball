8209bc57a0dbf55f9cee61a6fba523b5b1eb6b3c
Gerald Bauer <gerald.bauer@gmail.com>
2024-10-03 18:13:53 +0200

    up

--- sandbox/helper.rb
+++ sandbox/helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift( '../../../rubycocos/webclient/webget/lib' )
-$LOAD_PATH.unshift( '../timezones/lib' )
+$LOAD_PATH.unshift( '../sport.db.more/timezones/lib' )
 $LOAD_PATH.unshift( './lib' )
 require 'worldfootball'
 
